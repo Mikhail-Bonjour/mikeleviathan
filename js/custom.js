aa8f307a8a58c8e32ee15a370bc5448e6aa9089a
Mikhail Bonjour <35705469+Mikhail-Bonjour@users.noreply.github.com>
2026-08-30 04:48:40 -0600

    Added html + css + js update
    Added html + css + js update

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+	
+	"use strict";
+	// Your custom js code goes here.
+
+});
+
+var d = new Date();
+document.getElementById("currentYear").innerHTML = d.getFullYear();
